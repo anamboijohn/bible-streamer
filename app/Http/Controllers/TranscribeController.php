daf39bde4dc0efd7781415fb2e332dd70ddc83da
anamboijohn <anamboijohn@gmail.com>
2025-02-14 23:03:27 +0000

    implement bible verse extraction

--- app/Http/Controllers/TranscribeController.php
+++ app/Http/Controllers/TranscribeController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\TranscribeRequest;
 use FFMpeg\FFMpeg;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
+use LucianoTonet\GroqPHP\Groq;
 
 class TranscribeController extends Controller
 {
@@ -22,6 +22,12 @@ class TranscribeController extends Controller
         $ffmpeg->open($audioPath)
             ->save(new \FFMpeg\Format\Audio\Mp3(), $mp3Path);
 
+        $transcription = $this->transcribeAudio($mp3Path);
+        logger($transcription);
+    }
+
+    private function transcribeAudio($mp3Path)
+    {
         // Send the MP3 file for transcription
         $apiUrl = 'https://api.groq.com/openai/v1/audio/transcriptions';
 
@@ -35,11 +41,38 @@ class TranscribeController extends Controller
                 'response_format' => 'json',
             ]);
 
-        logger($response->json());
-
         // Delete the MP3 file
         unlink($mp3Path);
 
-        return response()->json(['transcription' => $response->json('text')]);
+        return $response->json('text');
+    }
+
+    public function handdleLLMQuery(Request $request)
+    {
+        $data = $request->validate([
+            'text' => 'required|string',
+        ]);
+
+        $bibleVerse = $this->extractBibleVerses($data['text']);
+    }
+
+    private function extractBibleVerses($text)
+    {
+        $groq = new Groq();
+        $response = $groq->chat()->completions()->create([
+            'model' => 'llama3-8b-8192',
+            'messages' => [
+                [
+                    'role' => 'system',
+                    'content' => 'You are to extract Bilbe Verses from a text. The text is an audio transcription. You must return the verses in the format: \nFORMAT `"Book Chapter:Verse" or "Book Chapter:Verse-Chapter:Verse" or "Book chapter:verse-verse"`\n. If no verses are found, return strictly with NO \n If a specific Translation of the bible was mensioned note it in your response like this example: EXAMPLE `NIV Luke 1:9 or KJV Luke 1:9 or NWT Luke 1:9`. \nYou must return either the verse or NO and nothing else'
+                ],
+                [
+                    'role' => 'user',
+                    'content' => 'Extract Bible Verses from the following context Only: \n[Context: ' . $data['text'] . ']. \nExample: "God spke to us again and again. If we look at the book of genesis there we find how god created us. specifically the chapters 2 and when you look at the verse 7" is extracted as "Genesis 2:7"'
+                ]
+            ],
+        ]);
+
+        return  $response['choices'][0]['message']['content'];
     }
 }
